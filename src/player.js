6e4bf043c075c27ac0ce3d97a5b3687068d47b6d
AlvaroPlaza <57074350+AlvaroPlaza@users.noreply.github.com>
2022-02-24 15:04:09 +0100

    personaje

--- src/player.js
+++ src/player.js
@@ -15,13 +15,12 @@ export default class Player extends Phaser.GameObjects.Sprite {
     super(scene, x, y, 'player');
     this.score = 0;
     this.scene.add.existing(this);
-    this.scale.x = 10;
-    this.scale.y = 10;
     this.scene.physics.add.existing(this);
     // Queremos que el jugador no se salga de los límites del mundo
     this.body.setCollideWorldBounds();
     this.speed = 300;
     this.jumpSpeed = -400;
+    this.setScale(.2);
     // Esta label es la UI en la que pondremos la puntuación del jugador
     this.label = this.scene.add.text(10, 10, "");
     this.cursors = this.scene.input.keyboard.createCursorKeys();
