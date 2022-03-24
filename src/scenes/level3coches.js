a0f3d68d14d7eb126be566d6c708eae5f8bf2a63
Marcos <marcdela@ucm.es>
2022-03-24 20:32:46 +0100

    Update level3coches.js

--- src/scenes/level3coches.js
+++ src/scenes/level3coches.js
@@ -2,6 +2,20 @@ import Platform from '../sprites/platform.js';
 import Player from '../sprites/player.js';
 import Calabaza from '../sprites/calabaza.js';
 
+/*
+const createAligned = (scene, totalWidth, texture, scrollFactor) => {
+  const w = scene.textures.get(texture).getSourceImage().width;
+  const count = Math.ceil(totalWidth / w) * scrollFactor;
+  let x = 0;
+  for(let i = 0; i < count; i++){
+    const c = scene.add.image(x, scene.scale.height, texture)
+      .setOrigin(0,1)
+      .setScrollFactor(scrollFactor);
+
+    x += c.width;
+  }
+}
+*/
 
 /**
  * Escena principal del juego. La escena se compone de una serie de plataformas 
@@ -25,36 +39,33 @@ export default class Level3 extends Phaser.Scene {
   }
 
   preload(){
-    this.game.load.image('fondo1carreras', 'assets/fondo1carreras.png');
-    this.game.load.image('fondo2carreras', 'assets/fondo2carreras.png');
-    this.game.load.image('fondo3carreras', 'assets/fondo3carreras.png');  }
+    this.load.image('fondo1carreras', 'assets/sprites/fondo1carreras.png');
+    this.load.image('fondo2carreras', 'assets/sprites/fondo2carreras.png');
+    this.load.image('fondo3carreras', 'assets/sprites/fondo3carreras.png');  }
   /**
    * Creación de los elementos de la escena principal de juego
    */
   create() {
 
-    //Enable Arcade Physics
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    //var image = scene.add.tileSprite(x, y, width, height, textureKey);
+    
+    const width = this.scale.width;
+    const height = this.scale.height;
+    const totalWidth = width * 3;
+    const totalheight = height * 2;
 
-    this.fondo1carreras = this.game.add.tileSprite(0,
-    this.game.height - this.game.cache.getImage('fondo1carreras').height,
-    this.game.width,
-    this.game.cache.getImage('fondo1carreras').height,
-    'fondo1carreras'
-    );
-    this.fondo2carreras = this.game.add.tileSprite(0,
-    this.game.height - this.game.cache.getImage('fondo2carreras').height,
-    this.game.width,
-    this.game.cache.getImage('fondo2carreras').height,
-    'fondo2carreras'
-);
-this.fondo3carreras = this.game.add.tileSprite(0,
-    this.game.height - this.game.cache.getImage('fondo3carreras').height,
-    this.game.width,
-    this.game.cache.getImage('fondo3carreras').height,
-    'fondo3carreras'
-);
-    /*
+    this.parallax1 = this.add.tileSprite(0, 0, totalWidth, totalheight, 'fondo1carreras');
+    this.parallax2 = this.add.tileSprite(0, 0, totalWidth, totalheight,  'fondo2carreras');
+    this.parallax3 = this.add.tileSprite(0, 0, totalWidth, totalheight, 'fondo3carreras');
+
+
+
+
+
+
+
+
+    /* antiguo1
     let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'fondocoches');
     let scaleX = this.cameras.main.width / image.width;
     let scaleY = this.cameras.main.height / image.height;
@@ -74,31 +85,72 @@ this.fondo3carreras = this.game.add.tileSprite(0,
       }
     });
     */
+    
+    /* ANTIGUO 2
+    const width = this.scale.width;
+    const height = this.scale.height;
+    const totalWidth = width * 3;
 
-    let exit = this.add.image(this.cameras.main.width - 20, 20, "exit").setInteractive();
-    exit.on('pointerdown', function (ptr) { this.setScale(0.9, 0.9) } );
-    exit.on('pointerup', () => {
-      this.scene.start("lobby");
+    createAligned(this, totalWidth, 'fondo1carreras', 0.80);
+    this.cameras.main.setBounds(0,0, totalWidth, height);
+
+    this.exit = this.add.image(this.cameras.main.width - 20, 20, "exit").setInteractive();
+    this.exit.on('pointerdown', function (ptr) { this.setScale(0.9, 0.9) } );
+    this.exit.on('pointerup', () => {
+    this.scene.start("lobby");
+
     });
 
+    this.fullScreen = this.add.image(this.cameras.main.width - 50, 20, "fullScreen").setInteractive();
+    this.fullScreen.on('pointerdown', function (ptr) { this.setScale(0.9, 0.9) } );
+    this.fullScreen.on('pointerup', () => {
+      if (this.scale.isFullscreen){
+        this.fullScreen.setTexture("fullScreen");
+        this.scale.stopFullscreen();
+      }
+      else{
+        this.fullScreen.setTexture("fullScreen2");
+          this.scale.startFullscreen();
+      }
+    });
+    */
+    
 
     //this.bases = this.add.group();
     this.player = new Player(this, 500, 500);
-    this.calabaza = new Calabaza(this,0,500);
+    //this.calabaza = new Calabaza(this,0,500);
 
-    new Platform(this, this.player, 500, 350);
-    new Platform(this, this.player, 850, 250);
-    new Platform(this, this.player, 500, 150);
-    new Platform(this, this.player, 150, 250);
+    //plataformas
+    //new Platform(this, this.player, 500, 350);
+    //new Platform(this, this.player, 850, 250);
+    //new Platform(this, this.player, 500, 150);
+    //new Platform(this, this.player, 150, 250);
     //this.spawn();
 
   }
 
   update(){
-    this.fondo1carreras.tilePosition.x -= 0.05;
-    this.fondo2carreras.tilePosition.x -= 0.3;
-    this.fondo3carreras.tilePosition.x -= 0.75;
+    
 
+    //const cam = this.cameras.main;
+    //const speed = 3;
+    if(!this.player.muerte){
+      if(this.player.cursors.left.isDown){
+        //cam.scrollX -= speed;
+        this.parallax1.tilePositionX -= 0.15;
+        this.parallax2.tilePositionX -= 0.40;
+        this.parallax3.tilePositionX -= 1.10;
+      }
+      else if (this.player.cursors.right.isDown){
+       // cam.scrollX += speed;
+        this.parallax1.tilePositionX += 0.15;
+        this.parallax2.tilePositionX += 0.40;
+        this.parallax3.tilePositionX += 1.10;
+      }
+    }
+
+   // this.exit.setPosition(this.cameras.main.width - 20, 20);
+  //this.fullScreen.setPosition(this.cameras.main.width - 50, 20);
   }
 
   /**
